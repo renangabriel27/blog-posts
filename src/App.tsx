fa0d544bfdd0d8bd40fa441114fab7c16edc3715
Renan Gabriel <renanthompsom@gmail.com>
2020-08-04 20:04:37 -0300

    Add home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 
+import Home from './pages/Home';
 import GlobalStyle from './styles/global';
 
 const App: React.FC = () => (
   <>
-    <h1>Blog</h1>
+    <Home />
     <GlobalStyle />
   </>
 );
