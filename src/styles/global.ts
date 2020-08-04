b08320fa398af97f7221eb8dfb3806010f448f61
Renan Gabriel <renanthompsom@gmail.com>
2020-08-04 19:59:33 -0300

    Add global style

---
+++ src/styles/global.ts
@@ -0,0 +1,29 @@
+import { createGlobalStyle } from 'styled-components';
+
+export default createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    outline: 0;
+  }
+
+  body {
+    background: #23292e;
+    color: #fff;
+    -webkit-font-smoothing: antiliased;
+  }
+
+  body, input, button {
+    font-family: 'Lato', sans-serif;
+    font-size: 16px;
+  }
+
+  h1, h2, h3, h4, h5, h6, strong {
+    font-weight: 700;
+  }
+
+  button {
+    cursor: pointer;
+  }
+`;
