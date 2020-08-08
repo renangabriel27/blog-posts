b15ecf1f4ae5c848e27ce58cb5440edded414f92
Renan Gabriel <renanthompsom@gmail.com>
2020-08-08 19:01:52 -0300

    Add comments ket to constants

--- src/contants/local-storage.ts
+++ src/contants/local-storage.ts
@@ -3,6 +3,7 @@ import { AuthContext } from '../hooks/auth';
 
 const KEY_PREFIX = '@Blog';
 export const USERS_KEY = `${KEY_PREFIX}::users`;
+export const COMMENTS_KEY = `${KEY_PREFIX}::comments`;
 
 export const POSTS_KEY = () => {
   const { user } = React.useContext(AuthContext);
